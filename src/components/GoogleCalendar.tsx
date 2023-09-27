4e3050227afbd7be461d8b15392009e7eecedd9d
godfrey <joyyujiepeng@gmail.com>
2023-09-27 17:16:52 +0100

    feat: render all the dates grid

--- src/components/GoogleCalendar.tsx
+++ src/components/GoogleCalendar.tsx
@@ -7,6 +7,7 @@ import {
   startOfMonth,
   endOfMonth,
   format,
+  isSameMonth,
 } from "date-fns"
 
 interface DateProps {
@@ -57,28 +58,35 @@ function GoogleCalendar({ value }: DateProps) {
           </span>
         </div>
         <div className="days">
-          <div className="day non-month-day old-month-day">
-            <div className="day-header">
-              <div className="week-name">Sun</div>
-              <div className="day-number">28</div>
-              <button className="add-event-btn">+</button>
+          {visibleDates.map((day) => (
+            // old-month-day non-month-day
+            <div
+              className={`day ${
+                !isSameMonth(day, visibleMonth) && "non-month-day"
+              }`}
+            >
+              <div className="day-header">
+                <div className="week-name">{format(day, "E")}</div>
+                <div className="day-number">{format(day, "d")}</div>
+                <button className="add-event-btn">+</button>
+              </div>
+              {/* <div className="events">
+             <button className="all-day-event blue event">
+               <div className="event-name">Short</div>
+             </button>
+             <button className="all-day-event green event">
+               <div className="event-name">
+                 Long Event Name That Just Keeps Going
+               </div>
+             </button>
+             <button className="event">
+               <div className="color-dot blue"></div>
+               <div className="event-time">7am</div>
+               <div className="event-name">Event Name</div>
+             </button>
+           </div> */}
             </div>
-            <div className="events">
-              <button className="all-day-event blue event">
-                <div className="event-name">Short</div>
-              </button>
-              <button className="all-day-event green event">
-                <div className="event-name">
-                  Long Event Name That Just Keeps Going
-                </div>
-              </button>
-              <button className="event">
-                <div className="color-dot blue"></div>
-                <div className="event-time">7am</div>
-                <div className="event-name">Event Name</div>
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
